4b80d9b8f1de6d7bc351268f722f427229d01f3a
Steven E. Newton <cratermoon@gmail.com>
2016-07-29 07:50:58 -0700

    got war ending properly, now to fix the card counts

--- src/main/java/com/cmdev/war/OpenWarfare.java
+++ src/main/java/com/cmdev/war/OpenWarfare.java
@@ -12,43 +12,54 @@ public class OpenWarfare
 	{
 		System.out.println("Let's have a war!");
 		// just like battle except for 2 cards at a time?
-		ListIterator<Player> playerIterator = players.listIterator();
 	
 		// Prime the loop
-		Player roundWinner = playerIterator.next();
+		Player roundWinner = players.get(0);
 		Card winningCard = roundWinner.topCard();
 		Card downCard = roundWinner.topCard();
 		cardsPlayedThisRound.add(winningCard);
 		cardsPlayedThisRound.add(downCard);
-		while(players.size() > 1 && playerIterator.hasNext())
+		warsFought = 0;
+		boolean atWar = true;
+		while (atWar)
 		{
-			Player currentPlayer = playerIterator.next();
-			if (currentPlayer.hasMoreCards())
+			ListIterator<Player> playerIterator = players.listIterator();
+			while(players.size() > 1 && playerIterator.hasNext())
 			{
-				Card nextCard = currentPlayer.topCard();
-				downCard = currentPlayer.topCard();
-				cardsPlayedThisRound.add(nextCard);
-				cardsPlayedThisRound.add(downCard);
-				if (nextCard.compareTo(winningCard) > 0)
+				System.out.println(players.size()+ " armies");
+				System.out.println("War round "+ (++warsFought));
+				Player currentPlayer = playerIterator.next();
+				if (currentPlayer.hasMoreCards())
 				{
-					//System.out.println("Player "+currentPlayer+ " wins");
-					winningCard = nextCard;
-					roundWinner = currentPlayer;
-				} else if (nextCard.compareTo(winningCard) == 0)
-				{
-					// wartime
-					playerIterator.remove();
+					Card nextCard = currentPlayer.topCard();
+					downCard = currentPlayer.topCard();
+					cardsPlayedThisRound.add(nextCard);
+					cardsPlayedThisRound.add(downCard);
+					if (nextCard.compareTo(winningCard) > 0)
+					{
+						System.out.println("Player "+currentPlayer+ " wins the war!");
+						atWar = false;
+						winningCard = nextCard;
+						roundWinner = currentPlayer;
+					} else if (nextCard.compareTo(winningCard) == 0)
+					{
+						// wartime
+						playerIterator.remove();
+					} else {
+						atWar = false;
+						System.out.println("Player "+roundWinner+ " wins the war");
+					}
 				} else {
-					System.out.println("Player "+roundWinner+ " wins");
+					System.out.println("Removing player with no cards");
+					playerIterator.remove();
+					if (players.size() == 1)
+					{
+						atWar = false;
+					}
 				}
-			} else {
-				System.out.println("Removing player with no cards");
-				playerIterator.remove();
-			}
+			}			
 		}
 		roundWinner.addAll(cardsPlayedThisRound);
-		warsFought++;
-		System.out.println(warsFought + " battles");
 	}
 
 	public int getWarsFought()
--- src/test/java/com/cmdev/war/WarfareTest.java
+++ src/test/java/com/cmdev/war/WarfareTest.java
@@ -28,5 +28,35 @@ public class WarfareTest
 		assertEquals("Too many wars!", 1, warfare.getWarsFought());
 		assertEquals("Wrong number of cards go to winner", 4, currentRoundCards.size());
 	}
+
+	@Test
+	public void testProlongedWar()
+	{
+		OpenWarfare warfare = new OpenWarfare();
+		List<Player> playTesters = new ArrayList<Player>(2);
+		Player player1 = new Player(1);
+		// first round
+		player1.addCard(new Card(1,2));
+		player1.addCard(new Card(2,5));
+		// second round
+		player1.addCard(new Card(2,12));
+		player1.addCard(new Card(3,12));
+
+		Player player2 = new Player(2);
+		// first round
+		player2.addCard(new Card(2,2));
+		player2.addCard(new Card(3,5));
+		// second round
+		player2.addCard(new Card(1,12));
+		player2.addCard(new Card(3,12));
+
+		playTesters.add(player1);
+		playTesters.add(player2);
+		List<Card> currentRoundCards = new ArrayList<Card>();
+		warfare.goToWar(playTesters, currentRoundCards);
+		assertEquals("Wrong number of wars!", 2, warfare.getWarsFought());
+		assertEquals("Wrong number of cards go to winner", 4, currentRoundCards.size());
+
+	}
 	
 }
